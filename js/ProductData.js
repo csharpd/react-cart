78590af76800b200e4b659500a3861ed335bae93
csharpd <chloedonegan@gmail.com>
2017-03-15 17:02:09 +0000

    flux shopping cart clinque

--- js/ProductData.js
+++ js/ProductData.js
@@ -5,27 +5,27 @@ module.exports = {
     localStorage.setItem('product', JSON.stringify([
       {
         id: '0011001',
-        name: 'Scotch.io Signature Lager',
-        image: 'scotch-beer.png',
-        description: 'The finest lager money can buy. Hints of keyboard aerosol, with a whiff of iKlear wipes on the nose. If you pass out while drinking this beverage, Chris Sevilleja personally tucks you in.',
+        name: 'Dramatically Different Moisturizing Lotion+',
+        image: 'clinique.png',
+        description: 'Our new formula now helps strengthen skins own moisture barrier. More moisture stays in.  Skin feels soft, springy. Has a healthy-looking glow. ',
         variants: [
           {
             sku: '123123',
-            type: '40oz Bottle',
-            price: 4.99,
-            inventory: 1
+            type: '50ml (Tube)',
+            price: 18.00,
+            inventory: 2
 
           },
           {
             sku: '123124',
-            type: '6 Pack',
-            price: 12.99,
-            inventory: 5
+            type: '125ml (With cap)',
+            price: 29.00,
+            inventory: 2
           },
           {
             sku: '1231235',
-            type: '30 Pack',
-            price: 19.99,
+            type: '125ml (With pump)',
+            price: 30.00,
             inventory: 3
           }
         ]
